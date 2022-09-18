794d8667044ec99eb2bbea6530d74279d0308b98
misomazf88 <ma.zapataf@uniandes.edu.co>
2022-09-18 02:51:48 -0500

    Adicion metodos crud para relacion ciudad supermercados

--- src/ciudad-supermercado/ciudad-supermercado.service.ts
+++ src/ciudad-supermercado/ciudad-supermercado.service.ts
@@ -33,6 +33,72 @@ export class CiudadSupermercadoService {
     if (!ciudad)
       throw new BusinessLogicException("No se encontró la ciudad con la identificación proporcionada.", BusinessError.NOT_FOUND);
     ciudad.supermercados = [...ciudad.supermercados, supermercado];
-    return await this.ciudadRepository.save(supermercado);
+    return await this.ciudadRepository.save(ciudad);
+  }
+
+  /**
+  * Obtiene los supermercados que tiene una ciudad.
+  * @param ciudadId
+  * @returns SupermercadoEntity[]
+  */
+  async findSupermarketsFromCity(ciudadId: string): Promise<SupermercadoEntity[]> {
+    const ciudad: CiudadEntity = await this.ciudadRepository.findOne({ where: { id: ciudadId }, relations: ["supermercados"] });
+    if (!ciudad)
+      throw new BusinessLogicException("No se encontró la ciudad con la identificación proporcionada.", BusinessError.NOT_FOUND)
+    return ciudad.supermercados;
+  }
+
+  /**
+  * Obtiene un supermercado de una ciudad.
+  * @param ciudadId
+  * @param supermercadoId
+  * @returns SupermercadoEntity
+  */
+  async findSupermarketFromCity(ciudadId: string, supermercadoId: string): Promise<SupermercadoEntity> {
+    const supermercado: SupermercadoEntity = await this.supermercadoRepository.findOne({ where: { id: supermercadoId } });
+    if (!supermercado)
+      throw new BusinessLogicException("No se encontró el supermercado con la identificación dada", BusinessError.NOT_FOUND)
+    const ciudad: CiudadEntity = await this.ciudadRepository.findOne({ where: { id: ciudadId }, relations: ["supermercados"] });
+    if (!ciudad)
+      throw new BusinessLogicException("No se encontró la ciudad con la identificación proporcionada.", BusinessError.NOT_FOUND)
+    const supermercadoCiudad: SupermercadoEntity = ciudad.supermercados.find(e => e.id === supermercado.id);
+    if (!supermercadoCiudad)
+      throw new BusinessLogicException("El supermercado con el id proporcionado no está asociado a la ciudad.", BusinessError.PRECONDITION_FAILED)
+    return supermercadoCiudad;
+  }
+
+  /**
+  * Actualiza los supermercados que tiene una ciudad.
+  * @param ciudadId
+  * @param supermercadoId
+  * @returns CiudadEntity
+  */
+  async updateSupermarketsFromCity(ciudadId: string, supermercados: SupermercadoEntity[]): Promise<CiudadEntity> {
+    const ciudad: CiudadEntity = await this.ciudadRepository.findOne({ where: { id: ciudadId }, relations: ["supermercados"] });
+    if (!ciudad)
+      throw new BusinessLogicException("No se encontró la ciudad con la identificación proporcionada.", BusinessError.NOT_FOUND)
+    for (let i = 0; i < supermercados.length; i++) {
+      const supermercado: SupermercadoEntity = await this.supermercadoRepository.findOne({ where: { id: `${supermercados[i].id}` } });
+      if (!supermercado)
+        throw new BusinessLogicException("No se encontró el supermercado con la identificación dada", BusinessError.NOT_FOUND)
+    }
+    ciudad.supermercados = supermercados;
+    return await this.ciudadRepository.save(ciudad);
+  }
+
+  async deleteSupermarketFromCity(ciudadId: string, supermercadoId: string) {
+    const supermercado: SupermercadoEntity = await this.supermercadoRepository.findOne({ where: { id: supermercadoId } });
+    if (!supermercado)
+      throw new BusinessLogicException("No se encontró el supermercado con la identificación dada", BusinessError.NOT_FOUND)
+    const ciudad: CiudadEntity = await this.ciudadRepository.findOne({ where: { id: ciudadId }, relations: ["supermercados"] });
+    if (!ciudad)
+      throw new BusinessLogicException("No se encontró la ciudad con la identificación proporcionada.", BusinessError.NOT_FOUND)
+    const ciudadSupermercado: SupermercadoEntity = ciudad.supermercados.find(e => e.id === supermercado.id);
+
+    if (!ciudadSupermercado)
+      throw new BusinessLogicException("El supermercado con el id proporcionado no está asociada a la ciudad.", BusinessError.PRECONDITION_FAILED)
+
+    ciudad.supermercados = ciudad.supermercados.filter(e => e.id !== supermercadoId);
+    await this.ciudadRepository.delete(ciudad);
   }
 }
