72475efce9abed58c31b47ce1d5bc1cd3c7c152f
misomazf88 <ma.zapataf@uniandes.edu.co>
2022-09-18 02:57:39 -0500

    Correccion modulo asociacion ciudad supermercado

--- src/ciudad-supermercado/ciudad-supermercado.module.ts
+++ src/ciudad-supermercado/ciudad-supermercado.module.ts
@@ -1,7 +1,11 @@
-import { Module } from '@nestjs/common';
+import { CiudadEntity } from 'src/ciudad/ciudad.entity';
 import { CiudadSupermercadoService } from './ciudad-supermercado.service';
+import { Module } from '@nestjs/common';
+import { SupermercadoEntity } from 'src/supermercado/supermercado.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CiudadEntity, SupermercadoEntity])],
   providers: [CiudadSupermercadoService]
 })
-export class CiudadSupermercadoModule {}
+export class CiudadSupermercadoModule { }
